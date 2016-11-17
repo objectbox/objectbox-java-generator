ba1dde88a2e68879dfa09b0339d899a09649933f
Markus <markus@greenrobot>
2016-11-17 11:44:22 +0100

    testIdSyncBasics: add properties

--- greendao-code-modifier/src/test/kotlin/org/greenrobot/idsync/IdSyncTest.kt
+++ greendao-code-modifier/src/test/kotlin/org/greenrobot/idsync/IdSyncTest.kt
@@ -3,6 +3,8 @@ package org.greenrobot.idsync
 import org.eclipse.jdt.core.dom.TypeDeclaration
 import org.greenrobot.greendao.codemodifier.ParsedEntity
 import org.greenrobot.greendao.codemodifier.ParsedProperty
+import org.greenrobot.greendao.codemodifier.Variable
+import org.greenrobot.greendao.codemodifier.VariableType
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
@@ -48,21 +50,35 @@ class IdSyncTest {
     }
 
     @Test
-    fun testIdSync() {
-        idSync!!
+    fun testIdSyncBasics() {
         val parsedEntities = ArrayList<ParsedEntity>()
         val properties = ArrayList<ParsedProperty>()
+        properties.add(createProperty("foo"))
+        properties.add(createProperty("bar"))
         val entity1 = createEntity(properties, "Entity1")
         parsedEntities.add(entity1)
         idSync!!.sync(parsedEntities)
 
         val model = idSync!!.justRead()!!
-        assertEquals(1, model.entities.size);
+        assertEquals(1, model.lastEntityId)
+
+        assertEquals(1, model.entities.size)
         val entity = model.entities.first()
-        assertEquals("Entity1", entity.name);
-        assertEquals(1, entity.id);
-        assertTrue(entity.refId > 0);
-        assertEquals(1, model.lastEntityId);
+        assertEquals("Entity1", entity.name)
+        assertEquals(1, entity.id)
+        assertTrue(entity.refId > 1)
+
+        assertEquals(2, entity.properties.size)
+        assertEquals("foo", entity.properties[0].name)
+        assertEquals(1, entity.properties[0].id)
+        assertTrue(entity.properties[0].refId > 1)
+        assertEquals(2, entity.properties[1].id)
+        assertEquals("bar", entity.properties[1].name)
+        assertTrue(entity.properties[1].refId > 1)
+    }
+
+    private fun createProperty(name: String): ParsedProperty {
+        return ParsedProperty(Variable(VariableType("java.lang.String", false, "String"), name))
     }
 
     private fun createEntity(properties: ArrayList<ParsedProperty>, name: String): ParsedEntity {
