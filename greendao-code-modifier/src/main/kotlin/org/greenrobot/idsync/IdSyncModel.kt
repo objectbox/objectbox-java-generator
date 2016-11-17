2f30f521093b0c93827d4b7c1022d9c5ea14a7b9
Markus <markus@greenrobot>
2016-11-17 15:52:25 +0100

    "Comments" in the JSON file

--- greendao-code-modifier/src/main/kotlin/org/greenrobot/idsync/IdSyncModel.kt
+++ greendao-code-modifier/src/main/kotlin/org/greenrobot/idsync/IdSyncModel.kt
@@ -1,6 +1,11 @@
 package org.greenrobot.idsync
 
 data class IdSyncModel(
+        // "Comments" in the JSON file
+        val _note1: String = "KEEP THIS FILE! Check it into a version control system (VCS) like git.",
+        val _note2: String = "ObjectBox manages crucial IDs for your object model. See docs for details.",
+        val _note3: String = "If you have VCS conflicts, you must resolve them according to ObjectBox docs.",
+
         val version: Long,
         val metaVersion: Int,
         val lastEntityId: Int,
