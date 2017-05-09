01b08d9b2488e41f7e221592aa5cb4c43f34f9dc
Markus <markus@greenrobot>
2017-05-09 15:46:14 +0200

    ParseRelationTest.convertToGeneratorModel #cherry

--- objectbox-code-modifier/src/test/kotlin/io/obectbox/codemodifier/ParseTestBase.kt
+++ objectbox-code-modifier/src/test/kotlin/io/obectbox/codemodifier/ParseTestBase.kt
@@ -1,6 +1,13 @@
 package io.obectbox.codemodifier
 
+import io.objectbox.codemodifier.GreendaoModelTranslator
+import io.objectbox.codemodifier.ParsedEntity
 import io.objectbox.codemodifier.VariableType
+import io.objectbox.generator.idsync.IdSync
+import io.objectbox.generator.model.Entity
+import io.objectbox.generator.model.Schema
+import java.io.File
+import java.util.*
 
 open class ParseTestBase {
 
@@ -10,5 +17,26 @@ open class ParseTestBase {
 
     fun parse(code: String, classesInPackage: List<String> = emptyList()) =
             tryParseEntity(code, classesInPackage)
+
+    /** Tests may opt to trigger additional tests via conversion */
+    fun convertToGeneratorModel(parsedEntity: ParsedEntity): Entity {
+        val parsedEntities = Collections.singletonList(parsedEntity)
+        val jsonFile = File.createTempFile("objectbox-model-test-", ".json")
+        try {
+            val idSync = IdSync(jsonFile)
+            idSync.sync(parsedEntities)
+
+            // take explicitly specified package name, or package name of the first entity
+            val packageName = parsedEntity.packageName
+            val schema = Schema("default", 1, packageName)
+            schema.lastEntityId = idSync.lastEntityId
+            schema.lastIndexId = idSync.lastIndexId
+            val mapping = GreendaoModelTranslator.convert(parsedEntities, schema, packageName, idSync)
+
+            return mapping[parsedEntity]!!
+        } finally {
+            jsonFile.delete()
+        }
+    }
 
 }
