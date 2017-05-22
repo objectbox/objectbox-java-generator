6549766f2025fb16eb90bc87e18a467f78a38d86
Uwe Trottmann <uwe@greenrobot.de>
2017-05-22 15:38:08 +0200

    Add ToOne test.

---
+++ objectbox-processor/src/test/resources/ToOneParentEntity.java
@@ -0,0 +1,20 @@
+package io.objectbox.processor.test;
+
+import java.util.Date;
+
+import io.objectbox.BoxStore;
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+import io.objectbox.relation.ToOne;
+
+@Entity
+public class ToOneParentEntity {
+
+    @Id long id;
+
+    ToOne<ToOneChildEntity> child = new ToOne<>(this, ToOneParentEntity_.child);
+
+    // need to add manually, as processor can not modify entity
+    transient BoxStore __boxStore;
+
+}
