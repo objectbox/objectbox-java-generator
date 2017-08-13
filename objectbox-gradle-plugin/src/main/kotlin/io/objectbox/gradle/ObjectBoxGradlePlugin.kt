4f6ab331e94b66900acc2998e1c10bb646d0ffc5
Markus <markus@greenrobot>
2017-08-13 17:35:32 +0100

    ObjectBoxGradlePlugin: reworked & refactored addDependenciesAnnotationProcessor

--- objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/ObjectBoxGradlePlugin.kt
+++ objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/ObjectBoxGradlePlugin.kt
@@ -10,8 +10,9 @@ class ObjectBoxGradlePlugin : Plugin<Project> {
         const val DEBUG = false
     }
 
+    val buildTracker = GradleBuildTracker("GradlePlugin")
+
     override fun apply(project: Project) {
-        val buildTracker = GradleBuildTracker("GradlePlugin")
         try {
             val env = ProjectEnv(project)
             if (!env.hasAndroidPlugin) {
@@ -20,34 +21,15 @@ class ObjectBoxGradlePlugin : Plugin<Project> {
                         "There is NO TRANSFORM SUPPORT for plain Java/Kotlin projects yet. " +
                         "Without transformations, functionality is limited, e.g. relations are unsupported. ")
             }
-            addDependencies(env, project)
+            addDependenciesAnnotationProcessor(env)
+            addDependencies(env)
 
             // Cannot use afterEvaluate to register transform, thus our plugin must be applied after Android
             if (ObjectBoxAndroidTransform.Registration.getAndroidExtensionClasses(project).isNotEmpty()) {
                 ObjectBoxAndroidTransform.Registration.to(project)
             }
 
-            val task = project.task("objectboxVerifySetup")
-            if (DEBUG) println("### Created $task in $project")
-            var buildTask = project.tasks.findByName("preBuild") ?: project.tasks.getByName("build")
-            buildTask.dependsOn(task)
-            task.doFirst {
-                val taskEnv = ProjectEnv(project) // Now Options are available
-                if (DEBUG) println("### Executing $task in $project")
-                buildTracker.trackBuild(taskEnv)
-
-                if (ObjectBoxAndroidTransform.Registration.getAndroidExtensionClasses(project).isEmpty()) {
-                    // TODO check
-                }
-
-                val aptConf = project.configurations.findByName("kapt") ?:
-                        project.configurations.findByName("annotationProcessor") ?:
-                        project.configurations.findByName("apt")
-                val foundDependency = aptConf?.dependencies?.firstOrNull() { it.group == "io.objectbox" }
-                foundDependency ?:
-                        throw RuntimeException("No configuration found with the ObjectBox annotation processor. " +
-                                "Currently only Android projects are fully supported.")
-            }
+            createVerifySetupTask(env)
         } catch(e: Throwable) {
             if (e is TransformException) buildTracker.trackError("Transform preparation failed", e)
             else buildTracker.trackFatal("Applying plugin failed", e)
@@ -55,40 +37,71 @@ class ObjectBoxGradlePlugin : Plugin<Project> {
         }
     }
 
-    private fun addDependencies(env: ProjectEnv, project: Project) {
-        val pluginVersion = env.objectBoxVersion
-        val runtimeVersion = pluginVersion
+    private fun createVerifySetupTask(env: ProjectEnv) {
+        val project = env.project
+        val task = project.task("objectboxVerifySetup")
+        if (DEBUG) println("### Created $task in $project")
+        var buildTask = project.tasks.findByName("preBuild") ?: project.tasks.getByName("build")
+        buildTask.dependsOn(task)
+        task.doFirst {
+            val taskEnv = ProjectEnv(project) // Now Options are available
+            if (DEBUG) println("### Executing $task in $project")
+            buildTracker.trackBuild(taskEnv)
 
+//            if (ObjectBoxAndroidTransform.Registration.getAndroidExtensionClasses(project).isEmpty()) {
+//                // TODO check
+//            }
+
+            val aptConf = project.configurations.findByName("kapt") ?:
+                    project.configurations.findByName("annotationProcessor") ?:
+                    project.configurations.findByName("apt")
+            val foundDependency = aptConf?.dependencies?.firstOrNull() { it.group == "io.objectbox" }
+            if (foundDependency == null) {
+                var msg = "No ObjectBox annotation processor configuration found. Please check your build scripts."
+                if (!env.hasAndroidPlugin) msg += "Currently only Android projects are fully supported."
+                throw RuntimeException(msg)
+            }
+        }
+    }
+
+    private fun addDependenciesAnnotationProcessor(env: ProjectEnv) {
         // Does not seem to work with Android projects; project should do that themselves:
-        val processorDep = "io.objectbox:objectbox-processor:$pluginVersion"
-
-        val depScope = env.dependencyScopeApiOrCompile
-        if (env.hasKotlinPlugin) {
-            if (DEBUG) println("### Kotlin plugin detected")
-
+        val processorDep = "io.objectbox:objectbox-processor:${env.objectBoxVersion}"
+        val project = env.project
+        if (project.configurations.findByName("kapt") != null) {
+            project.dependencies.add("kapt", processorDep)
+        } else if (project.configurations.findByName("annotationProcessor") != null) {
+            // Android uses annotationProcessor
+            project.dependencies.add("annotationProcessor", processorDep)
+        } else if (project.configurations.findByName("apt") != null) {
+            // https://bitbucket.org/hvisser/android-apt or custom apt
+            // https://docs.gradle.org/current/userguide/java_plugin.html#sec:java_compile_avoidance
+            project.dependencies.add("apt", processorDep)
+        } else if (env.hasKotlinPlugin) {
             if (!project.plugins.hasPlugin("kotlin-kapt")) {
                 // Does not seem to work reliable; project should do that themselves:
                 project.plugins.apply("kotlin-kapt")
+                project.dependencies.add("kapt", processorDep)
                 if (DEBUG) println("### Kotlin KAPT plugin added")
             }
+        }
+    }
+
+    private fun addDependencies(env: ProjectEnv) {
+        val pluginVersion = env.objectBoxVersion
+        val runtimeVersion = pluginVersion
+        val depScope = env.dependencyScopeApiOrCompile
+        val project = env.project
+        if (env.hasKotlinPlugin) {
+            if (DEBUG) println("### Kotlin plugin detected")
             project.dependencies.add(depScope, "io.objectbox:objectbox-kotlin:$runtimeVersion")
-            project.dependencies.add("kapt", processorDep)
+        }
+
+        if (env.hasAndroidPlugin) {
+            project.dependencies.add(depScope, "io.objectbox:objectbox-android:$runtimeVersion")
+            project.dependencies.add("androidTestCompile", "com.google.code.findbugs:jsr305:3.0.2")
         } else {
-            // Gradle requires custom config!?
-            // https://docs.gradle.org/current/userguide/java_plugin.html#sec:java_compile_avoidance
-            // Android uses annotationProcessor
-            if (project.configurations.findByName("annotationProcessor") != null) {
-                project.dependencies.add("annotationProcessor", processorDep)
-            } else if (project.configurations.findByName("apt") != null) {
-                // https://bitbucket.org/hvisser/android-apt or custom apt
-                project.dependencies.add("apt", processorDep)
-            }
-            if (env.hasAndroidPlugin) {
-                project.dependencies.add(depScope, "io.objectbox:objectbox-android:$runtimeVersion")
-                project.dependencies.add("androidTestCompile", "com.google.code.findbugs:jsr305:3.0.2")
-            } else {
-                project.dependencies.add(depScope, "io.objectbox:objectbox-java:$runtimeVersion")
-            }
+            project.dependencies.add(depScope, "io.objectbox:objectbox-java:$runtimeVersion")
         }
     }
 
