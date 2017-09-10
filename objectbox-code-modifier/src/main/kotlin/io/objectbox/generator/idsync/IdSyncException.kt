605b51079589d3da7346a6e51147d0d129278744
Markus <markus@greenrobot>
2017-09-10 21:55:48 +0100

    Do not use new line for IdSyncPrintUidException because message gets cut off

--- objectbox-code-modifier/src/main/kotlin/io/objectbox/generator/idsync/IdSyncException.kt
+++ objectbox-code-modifier/src/main/kotlin/io/objectbox/generator/idsync/IdSyncException.kt
@@ -4,6 +4,6 @@ open class IdSyncException(message: String? = null, cause: Throwable?=null) : Ru
 }
 
 class IdSyncPrintUidException(uidTarget:String, currentUid: Long, val randomNewUid: Long):
-        IdSyncException("UID operations for $uidTarget:\n" +
-                "[Rename] apply the current UID using @Uid(${currentUid}L)\n" +
+        IdSyncException("UID operations for $uidTarget: " +
+                "[Rename] apply the current UID using @Uid(${currentUid}L) - " +
                 "[Change/reset] apply a new UID using @Uid(${randomNewUid}L)")
