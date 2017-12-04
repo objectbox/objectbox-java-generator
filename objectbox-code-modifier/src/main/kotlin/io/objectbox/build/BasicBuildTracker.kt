d7a3ba8c4d5915796486b62f7257b872dcacb502
Uwe Trottmann <uwe@greenrobot.de>
2017-12-04 08:35:59 +0100

    1.3.4-SNAPSHOT

--- objectbox-code-modifier/src/main/kotlin/io/objectbox/build/BasicBuildTracker.kt
+++ objectbox-code-modifier/src/main/kotlin/io/objectbox/build/BasicBuildTracker.kt
@@ -98,7 +98,7 @@ open class BasicBuildTracker(val toolName: String) {
         return event.toString()
     }
 
-    open protected fun version(): String? = "1.3.3" // TODO replace with generated property, or update frequently...
+    open protected fun version(): String? = "1.3.4" // TODO replace with generated property, or update frequently...
 
     // public for tests in another module
     fun errorProperties(message: String?, throwable: Throwable?): String {
