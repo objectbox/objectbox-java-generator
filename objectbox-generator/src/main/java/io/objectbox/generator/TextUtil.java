8dfff26a685074468103ab57a2fe802942b427f8
Markus <markus@greenrobot>
2018-01-26 14:23:57 +0100

    added copyright headers to Java and Kotlin sources

--- objectbox-generator/src/main/java/io/objectbox/generator/TextUtil.java
+++ objectbox-generator/src/main/java/io/objectbox/generator/TextUtil.java
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2011-2016 Markus Junginger, greenrobot (http://greenrobot.org)
+ * Copyright (C) 2017-2018 ObjectBox Ltd.
  *
- * This file is part of greenDAO Generator.
+ * This file is part of ObjectBox Build Tools.
  *
- * greenDAO Generator is free software: you can redistribute it and/or modify
+ * ObjectBox Build Tools is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * greenDAO Generator is distributed in the hope that it will be useful,
+ * ObjectBox Build Tools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with greenDAO Generator.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ObjectBox Build Tools.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package io.objectbox.generator;
