6515a0d85b86a562d8f5017f99b3eb29740b7411
Uwe Trottmann <uwe@greenrobot.de>
2019-09-23 14:59:48 +0200

    ClassTransformer: decode ObjectBox location URL to path name.
    Follow-up to `7a27b007 0.9.16-RC2, replace %20 with space for javassist classpath if required`.
    
    https://github.com/objectbox/objectbox-java/issues/135

--- objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/transform/ClassTransformer.kt
+++ objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/transform/ClassTransformer.kt
@@ -32,6 +32,7 @@ import javassist.bytecode.SignatureAttribute
 import javassist.expr.ExprEditor
 import javassist.expr.FieldAccess
 import java.io.File
+import java.net.URLDecoder
 
 /**
  * Transforms entity class files: adds a BoxStore field and adds relation field (ToOne, ToMany) initialization to
@@ -54,17 +55,9 @@ class ClassTransformer(val debug: Boolean = false) {
             // 3) ObjectBox is separated for mainly for tests (we make this configurable, treat tests as special)
             // 4) Don't fake java.lang.Object, it may cause stack overflows because superclass != null
             val objectBoxPath = BoxStoreBuilder::class.java.protectionDomain.codeSource.location.path
-            try {
-                classPool.appendClassPath(objectBoxPath)
-            } catch (e: NotFoundException) {
-                if (objectBoxPath.contains("%20")) {
-                    // Spaces in user path can be problematic (at least on Windows).
-                    // See https://github.com/greenrobot/ObjectBox/issues/135#issuecomment-325219079
-                    classPool.appendClassPath(objectBoxPath.replace("%20", " "))
-                } else {
-                    throw e
-                }
-            }
+            // location.path is a URL, but javassist expects a path: so decode the URL first.
+            val decodedObjectBoxPath = URLDecoder.decode(objectBoxPath, "UTF-8")
+            classPool.appendClassPath(decodedObjectBoxPath)
             classPool.appendClassPath(PrefixedClassPath("java.", java.lang.Object::class.java))
         }
 
