d795e4969321e240333ffee5a835b226c0720432
Uwe Trottmann <uwe@greenrobot.de>
2020-11-10 12:13:35 +0100

    Use classifiers: native sync library defined via classifier (was version name).

--- objectbox-gradle-plugin/src/test/kotlin/io/objectbox/gradle/SyncPluginApplyTest.kt
+++ objectbox-gradle-plugin/src/test/kotlin/io/objectbox/gradle/SyncPluginApplyTest.kt
@@ -1,5 +1,9 @@
 package io.objectbox.gradle
 
+import org.gradle.api.artifacts.DependencySet
+import org.gradle.api.artifacts.ModuleDependency
+import org.junit.Assert
+
 
 /**
  * Tests applying [ObjectBoxSyncGradlePlugin] configures project as expected.
@@ -7,6 +11,24 @@ package io.objectbox.gradle
 class SyncPluginApplyTest : PluginApplyTest() {
 
     override val pluginId = "io.objectbox.sync"
-    override val expectedNativeLibVersion = ProjectEnv.Const.nativeSyncVersionToApply
+    private val expectedClassifier = "sync"
+    override val expectedNativeLibVersion = ProjectEnv.Const.nativeSyncVersionToApply.removeSuffix(":$expectedClassifier")
+
+    override fun assertNativeDependency(compileDeps: DependencySet) {
+        Assert.assertEquals(1, compileDeps.count {
+            it.group == "io.objectbox"
+                    && (it.name == "objectbox-linux" || it.name == "objectbox-windows" || it.name == "objectbox-macos")
+                    && it.version == expectedNativeLibVersion
+                    && it is ModuleDependency && it.artifacts.first().classifier == expectedClassifier
+        })
+    }
+
+    override fun assertAndroidDependency(deps: DependencySet) {
+        Assert.assertEquals(1, deps.count {
+            it.group == "io.objectbox" && it.name == "objectbox-android"
+                    && it.version == expectedNativeLibVersion
+                    && it is ModuleDependency && it.artifacts.first().classifier == expectedClassifier
+        })
+    }
 
 }
