5b5d28f8341787960b0afaee93f85cbe6ec0874f
Uwe Trottmann <uwe@greenrobot.de>
2020-11-30 11:09:18 +0100

    Gradle 6.7 follow-up: incremental comp. appears to count anonymous classes.

--- objectbox-gradle-plugin/src/test/kotlin/io/objectbox/gradle/IncrementalCompilationTest.kt
+++ objectbox-gradle-plugin/src/test/kotlin/io/objectbox/gradle/IncrementalCompilationTest.kt
@@ -78,8 +78,7 @@ class IncrementalCompilationTest {
         // Compile 2nd time.
         with(compileJava()) {
             assertThat(output).doesNotContain(GRADLE_MSG_FULL_RECOMPILE_REQ)
-            // Why 4 classes? Example.java, Example_.java, ExampleCursor.java, MyObjectBox.java.
-            // TODO Why 5 classes? Not sure, yet. But it's still incremental, so fine?
+            // Why 5 classes? Example.java, Example_.java, ExampleCursor.java + anonym. Factory class, MyObjectBox.java.
             assertThat(output).contains("Incremental compilation of 5 classes completed")
             assertThat(output).contains("[ObjectBox] Valid all-args constructor found")
         }
@@ -138,8 +137,7 @@ class IncrementalCompilationTest {
         // Compile 2nd time.
         with(compileJava()) {
             assertThat(output).doesNotContain(GRADLE_MSG_FULL_RECOMPILE_REQ)
-            // Why 4 classes? Example.java, Example_.java, ExampleCursor.java, MyObjectBox.java.
-            // TODO Why 5 classes? Not sure, yet. But it's still incremental, so fine?
+            // Why 5 classes? Example.java, Example_.java, ExampleCursor.java + anonym. Factory class, MyObjectBox.java.
             assertThat(output).contains("Incremental compilation of 5 classes completed")
             assertThat(output).contains("[ObjectBox] Detected entity inheritance chain: Example->BaseExample")
         }
@@ -213,8 +211,7 @@ class IncrementalCompilationTest {
         // Compile 2nd time.
         with(compileJava()) {
             assertThat(output).doesNotContain(GRADLE_MSG_FULL_RECOMPILE_REQ)
-            // Why 4 classes? Example.java, Example_.java, ExampleCursor.java, MyObjectBox.java.
-            // TODO Why 5 classes? Not sure, yet. But it's still incremental, so fine?
+            // Why 5 classes? Example.java, Example_.java, ExampleCursor.java + anonym. Factory class, MyObjectBox.java.
             assertThat(output).contains("Incremental compilation of 5 classes completed")
             assertThat(output).contains("[ObjectBox] Detected entity inheritance chain: Example->BaseExample")
         }
