objectbox/objectbox-java-generator

commit 157521f282cef8cc381feff6eedeee1291700007

Kotlin: replace deprecated toLowerCase with lowercase.

objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/GradleBuildTracker.kt
@@ -24,6 +24,7 @@ import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
 import io.objectbox.reporting.BasicBuildTracker
 import org.gradle.api.plugins.ExtensionContainer
+import java.util.*
 import kotlin.reflect.KClass
 
 /**
@@ -101,7 +102,7 @@ open class GradleBuildTracker(toolName: String) : BasicBuildTracker(toolName) {
             // https://circleci.com/docs/1.0/environment-variables/
             System.getenv("CIRCLECI") != null -> "C"
             // https://documentation.codeship.com/pro/builds-and-configuration/steps/
-            System.getenv("CI_NAME")?.toLowerCase() == "codeship" -> "CS"
+            System.getenv("CI_NAME").lowercase(Locale.ROOT) == "codeship" -> "CS"
             System.getenv("CI") != null -> "Other"
             else -> null
         }

objectbox-code-modifier/src/main/kotlin/io/objectbox/generator/idsync/IdSync.kt
@@ -143,7 +143,7 @@ class IdSync(private val jsonFile: File = File("objectmodel.json")) {
                     uidHelper.addExistingId(entity.uid)
                     entity.properties.forEach { uidHelper.addExistingId(it.uid) }
                     entitiesReadByUid[entity.uid] = entity
-                    if (entitiesReadByName.put(entity.name.toLowerCase(), entity) != null) {
+                    if (entitiesReadByName.put(entity.name.lowercase(Locale.getDefault()), entity) != null) {
                         throw IdSyncException("Malformed model file \"${jsonFile.name}\": " +
                                 "duplicate entity name ${entity.name} - please edit the file to resolve the issue")
                     }
@@ -473,7 +473,7 @@ class IdSync(private val jsonFile: File = File("objectmodel.json")) {
                     if (newUidPool.contains(uid)) return null
                     else throw IdSyncException("No entity with UID $uid found")
         } else {
-            return entitiesReadByName[name.toLowerCase()]
+            return entitiesReadByName[name.lowercase(Locale.getDefault())]
         }
     }
 
@@ -489,8 +489,8 @@ class IdSync(private val jsonFile: File = File("objectmodel.json")) {
             check(filtered.size == 1) { "property name: $name, UID: $uid" }
             return filtered.first()
         } else {
-            val nameLowerCase = name.toLowerCase()
-            val filtered = entity.properties.filter { it.name.toLowerCase() == nameLowerCase }
+            val nameLowerCase = name.lowercase(Locale.getDefault())
+            val filtered = entity.properties.filter { it.name.lowercase(Locale.getDefault()) == nameLowerCase }
             check(filtered.size <= 1) { "size: ${filtered.size} property name: $name, UID: $uid" }
             return if (filtered.isNotEmpty()) filtered.first() else null
         }
@@ -509,8 +509,8 @@ class IdSync(private val jsonFile: File = File("objectmodel.json")) {
             check(filtered.size == 1) { "relation name: $name, UID: $uid" }
             return filtered.first()
         } else {
-            val nameLowerCase = name.toLowerCase()
-            val filtered = entity.relations.filter { it.name.toLowerCase() == nameLowerCase }
+            val nameLowerCase = name.lowercase(Locale.getDefault())
+            val filtered = entity.relations.filter { it.name.lowercase(Locale.getDefault()) == nameLowerCase }
             check(filtered.size <= 1) { "size: ${filtered.size} relation name: $name, UID: $uid" }
             return if (filtered.isNotEmpty()) filtered.first() else null
         }
@@ -604,13 +604,13 @@ class IdSync(private val jsonFile: File = File("objectmodel.json")) {
     private fun validateBeforeWrite(model: IdSyncModel) {
         val entityNames = mutableSetOf<String>()
         for (entity in model.entities) {
-            if (!entityNames.add(entity.name.toLowerCase())) {
+            if (!entityNames.add(entity.name.lowercase(Locale.getDefault()))) {
                 throw IdSyncException("Could not write model file \"${jsonFile.name}\" - verification failed: " +
                         "duplicate entity name \"${entity.name}\" (please report if you think this a bug)")
             }
             val propertyNames = mutableSetOf<String>()
             for (property in entity.properties) {
-                if (!propertyNames.add(property.name.toLowerCase())) {
+                if (!propertyNames.add(property.name.lowercase(Locale.getDefault()))) {
                     throw IdSyncException("Could not write model file \"${jsonFile.name}\" - verification failed: " +
                             "duplicate property name \"${property.name}\" in entity \"${entity.name}\" " +
                             "(please report if you think this a bug)")

objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/ProjectEnv.kt
@@ -48,7 +48,7 @@ class ProjectEnv(val project: Project) {
 
     val osName: String = System.getProperty("os.name")
     val is64Bit = System.getProperty("sun.arch.data.model") == "64"
-    private val osNameLowerCase = osName.toLowerCase(Locale.US)
+    private val osNameLowerCase = osName.lowercase(Locale.US)
     private val isLinux = osNameLowerCase.contains("linux")
     private val isMac = osNameLowerCase.contains("mac")
     private val isWindows = osNameLowerCase.contains("windows")

commit 695a0a1966b986a3ef50a9f815bf3391803de25c

Tracking: do not crash if CI_NAME env var is no set.
Regression from
157521f2 Kotlin: replace deprecated toLowerCase with lowercase.

objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/GradleBuildTracker.kt
@@ -102,7 +102,7 @@ open class GradleBuildTracker(toolName: String) : BasicBuildTracker(toolName) {
             // https://circleci.com/docs/1.0/environment-variables/
             System.getenv("CIRCLECI") != null -> "C"
             // https://documentation.codeship.com/pro/builds-and-configuration/steps/
-            System.getenv("CI_NAME").lowercase(Locale.ROOT) == "codeship" -> "CS"
+            System.getenv("CI_NAME")?.lowercase(Locale.ROOT) == "codeship" -> "CS"
             System.getenv("CI") != null -> "Other"
             else -> null
         }