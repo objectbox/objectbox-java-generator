a46abb9dabb8e33224642e65425e3c1eeea16ae6
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2023-05-02 14:42:37 +0200

    Update versions plugin [0.42.0 -> 0.46.0]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // https://github.com/ben-manes/gradle-versions-plugin/releases
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
     // https://github.com/gradle-nexus/publish-plugin/releases
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     // https://github.com/gmazzo/gradle-buildconfig-plugin/releases
