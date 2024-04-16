32011c41a3cd4a758677912ffd59efb6e8527f19
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2024-04-16 10:50:46 +0200

    AGPL: update generator info message

--- objectbox-generator/src/main/java/io/objectbox/generator/BoxGenerator.java
+++ objectbox-generator/src/main/java/io/objectbox/generator/BoxGenerator.java
@@ -53,7 +53,7 @@ public class BoxGenerator {
 
     public BoxGenerator() throws IOException {
         log("ObjectBox Generator");
-        log("Copyright 2017-2024 ObjectBox Ltd, objectbox.io. Licensed under GPL V3.");
+        log("Copyright 2017-2024 ObjectBox Ltd, objectbox.io. Licensed under GNU Affero General Public License, Version 3.");
         log("This program comes with ABSOLUTELY NO WARRANTY");
 
         Configuration config = getConfiguration(MYOBJECTBOX_FTL);
