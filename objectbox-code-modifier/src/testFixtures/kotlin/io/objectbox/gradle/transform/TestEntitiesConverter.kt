61fa324cde7e8428a38d61dd0b9d9787b577e65d
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2024-04-16 10:25:48 +0200

    AGPL: update source file license headers

--- objectbox-code-modifier/src/testFixtures/kotlin/io/objectbox/gradle/transform/TestEntitiesConverter.kt
+++ objectbox-code-modifier/src/testFixtures/kotlin/io/objectbox/gradle/transform/TestEntitiesConverter.kt
@@ -1,19 +1,19 @@
 /*
+ * ObjectBox Build Tools
  * Copyright (C) 2017-2024 ObjectBox Ltd.
  *
- * This file is part of ObjectBox Build Tools.
- *
- * ObjectBox Build Tools is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * ObjectBox Build Tools is distributed in the hope that it will be useful,
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with ObjectBox Build Tools.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 @file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS", "unused", "ClassName")
