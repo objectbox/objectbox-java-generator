e657dd3fc8536528bf3e624308d967eb7d12736b
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2024-04-16 10:22:29 +0200

    Javadoc: make errors break the build again

--- buildSrc/src/main/kotlin/objectbox-publish.gradle.kts
+++ buildSrc/src/main/kotlin/objectbox-publish.gradle.kts
@@ -5,13 +5,6 @@ plugins {
     id("signing")
 }
 
-// Make javadoc task errors not break the build.
-if (JavaVersion.current().isJava8Compatible) {
-    tasks.withType<Javadoc> {
-        isFailOnError = false
-    }
-}
-
 publishing {
     repositories {
         maven {
