7216f1bcc9a9c9179cb13d95555e73657621943e
Uwe <noreply+uwe@objectbox.io>
2026-02-03 13:17:27 +0100

    AndroidCompat: require Android Gradle Plugin 8.1 or higher
    Adjust AGP 7.3 tests to expect error message when applying.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ The plugin
 ## Minimum requirements to apply the plugin
 
 - Gradle `7.0` (see [GradleCompat](objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/util/GradleCompat.kt))
-- Android Plugin `4.1` (see [AndroidCompat](objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/util/AndroidCompat.kt))
+- Android Plugin `8.1` (see [AndroidCompat](objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/util/AndroidCompat.kt))
 - JDK 11
                    
 For JVM projects, the Java byte code transformer supports `@Entity` classes using up to Java 11 byte code. 
--- objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/util/AndroidCompat.kt
+++ objectbox-gradle-plugin/src/main/kotlin/io/objectbox/gradle/util/AndroidCompat.kt
@@ -20,7 +20,6 @@ package io.objectbox.gradle.util
 
 import com.android.build.api.AndroidPluginVersion
 import com.android.build.api.variant.AndroidComponentsExtension
-import io.objectbox.gradle.transform.AndroidPlugin34
 import io.objectbox.gradle.transform.AndroidPlugin72
 import io.objectbox.gradle.transform.AndroidPluginCompat
 import org.gradle.api.Project
@@ -28,20 +27,24 @@ import org.gradle.api.Project
 
 object AndroidCompat {
 
+    private const val ERROR_AGP_TOO_OLD = "The ObjectBox Gradle plugin requires Android Gradle Plugin 8.1 or newer"
+
     fun getPlugin(project: Project): AndroidPluginCompat {
         return try {
             getPluginByVersion(project)
         } catch (e: NoClassDefFoundError) {
             // Android Plugins before 7.0 do not have the AndroidComponentsExtension (or version API).
-            AndroidPlugin34()
+            error(ERROR_AGP_TOO_OLD)
         }
     }
 
     private fun getPluginByVersion(project: Project): AndroidPluginCompat {
         val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
         return when {
-            androidComponents.pluginVersion >= AndroidPluginVersion(7, 2, 0) -> AndroidPlugin72()
-            else -> AndroidPlugin34()
+            // While this plugin supports 7.2 or higher, the ObjectBox Android library requires at least 8.1 since
+            // release 4.2.0 (2025-03-04).
+            androidComponents.pluginVersion >= AndroidPluginVersion(8, 1, 0) -> AndroidPlugin72()
+            else -> error(ERROR_AGP_TOO_OLD)
         }
     }
 
