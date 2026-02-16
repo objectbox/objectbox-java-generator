396075bdeacd35cc5620cb94d59b837f26b3d8d8
Uwe <noreply+uwe@objectbox.io>
2026-02-16 14:16:51 +0100

    Build: don't use snapshots on main branch
    This is safe, because Maven artifacts are not published from main branch
    on CI.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,8 @@ buildscript {
     // see the release checklist in the objectbox repo.
     // If true won't build snapshots and removes version post fix (e.g. "-dev-SNAPSHOT"),
     // uses release versions of dependencies.
-    val isRelease = System.getenv("OBX_RELEASE") == "true"
+    // val isRelease = System.getenv("OBX_RELEASE") == "true"
+    val isRelease = true // On (public) main branch don't use snapshots (publishing is disabled in CI for main branch)
 
     val libsRelease = isRelease // e.g. diverge if plugin is still SNAPSHOT, but libs are already final
     val libsVersion = versionNumber + (if (libsRelease) "" else "-dev-SNAPSHOT")
